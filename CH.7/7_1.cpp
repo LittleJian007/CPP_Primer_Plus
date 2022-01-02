c484094979da46f09d84c8d2bc4a50588122bc2d
LittleJian <1096185130@qq.com>
2022-01-02 17:40:35 +0800

    更新

--- CH.7/7_1.cpp
+++ CH.7/7_1.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 using namespace std;
-void cout_1(int n) ;
+void cout_1(int n);
 
 int main()
 {
-int size = 0;
-cin >> size;
-       cout_1(size);
+    int size = 0;
+    cin >> size;
+    cout_1(size);
     // cin.get();
     // cin.get();
     return 0;
@@ -14,7 +14,7 @@ cin >> size;
 
 void cout_1(int n)
 {
-    for(int i = 1; i <= n; i++)
+    for (int i = 1; i <= n; i++)
         cout << " C++! " << i << endl;
-    return ;
+    return;
 }
