f204ac9bada8a557138fd489cc8c1f693af20d52
LittleJian <1096185130@qq.com>
2022-02-22 21:57:45 +0800

    CH.12 No.01

---
+++ CH.12/Inc/StringBad.h
@@ -0,0 +1,19 @@
+#ifndef STRINGBAD_H_
+#define STRINGBAD_H_
+#include <iostream>
+
+class StringBad
+{
+private:
+    char *str;
+    int len;
+    static int num_string;
+
+public:
+    StringBad(const char *s);
+    StringBad();
+    ~StringBad();
+    friend std::ostream &operator<<(std::ostream &os, const StringBad &st);
+};
+
+#endif
