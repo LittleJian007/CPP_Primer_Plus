56b018fcbd631ce194446d69884bd26b16d6bbab
007 <1096185130@qq.com>
2022-05-16 12:28:36 +0800

    add copy13.7

--- CH.13/13.7copy/usedma.cpp
+++ CH.13/13.7copy/usedma.cpp
@@ -6,10 +6,38 @@ int main()
 
     cout << "Playing baseDMA object:" << endl;
     cout << base1;
+    cout << "-------------------------\n";
 
-    baseDMA base2;
-    base2 = base1;
+    baseDMA base2 = base1; //复制函数
     cout << base2;
+    cout << "-------------------------\n";
+
+    baseDMA base3;
+    base3 = base1; //赋值函数
+    cout << base3;
+    cout << "-------------------------\n";
+
+    cout << "Playing lacksDMA object:" << endl;
+    lacksDMA laks1("Lacks1", 10, "red");
+    cout << laks1;
+    cout << "-------------------------\n";
+
+    lacksDMA laks2 = laks1;
+    cout << laks2;
+    cout << "-------------------------\n";
+
+    lacksDMA laks3;
+    laks3 = laks2;
+    cout << laks3;
+    cout << "-------------------------\n";
+
+    lacksDMA laks4(base1,"yello");
+    cout << laks4;
+    cout << "-------------------------\n";
+
+    cout << "Playing hasDMA object:" << endl;
+    hasDMA has1(base2, "keys");
+    cout << has1;
 
     return 0;
 }
